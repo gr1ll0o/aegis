d5a8433acf8bdae8ab83333ef0bd5a37b712d834
gr1ll0o <thiagotobias.grillo@gmail.com>
2025-09-12 22:25:52 -0300

    Scripts folder

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const { ipcRenderer } = require('electron');
+
+document.getElementById("min-btn").addEventListener("click", () => {
+  ipcRenderer.send("minimize-app");
+});
+
+bcrypt.hash("prueba1234", saltRounds, (error, hash) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log("Password: ", hash);
+});
+
+bcrypt.compare("prueba1234", "HASH", (error, response) => {
+    if (error) { console.log(error);return; }
+    console.log(response);
+});
